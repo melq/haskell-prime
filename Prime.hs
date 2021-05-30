66fbea5119581bdd151122c516e8b4a432f469a7
melq <tu2sice@yahoo.co.jp>
2021-05-30 17:49:13 +0900

    :sparkles: 時間計測機能を追加

--- Prime.hs
+++ Prime.hs
@@ -1,3 +1,13 @@
+import Data.Time
+
+main :: IO ()
+main = do
+  x <- getCurrentTime
+  let cnt = countPrime 1000000
+  y <- getCurrentTime
+  print cnt
+  print $ diffUTCTime y x
+
 countPrime :: Int -> Int
 countPrime n = length (filter isPrime [1 .. n])
 
